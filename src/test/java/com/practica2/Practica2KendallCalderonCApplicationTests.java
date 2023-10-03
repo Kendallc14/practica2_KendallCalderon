9eb641290052e30c281077a99783f40d5857a532
Kendall C <Kendall C@EquipoKendall>
2023-10-03 16:02:05 -0600

    Practica 2 del curso Desarrollo de web y patrones Kendall Calderón Calderón

---
+++ src/test/java/com/practica2/Practica2KendallCalderonCApplicationTests.java
@@ -0,0 +1,11 @@
+package com.practica2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2KendallCalderonCApplicationTests {
+    @Test
+	void contextLoads() {
+	}
+}
